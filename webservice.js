d1b4aece756d4ad7591bda46f682fb1cac1699ae
josalesmj <jo.salesmj@gmail.com>
2022-09-10 08:58:34 -0300

    feature: Adds route

--- webservice.js
+++ webservice.js
@@ -3,6 +3,19 @@ let fs = require('fs');
 let content = fs.readFileSync('./html/index.html');
 
 http.createServer(function(req,res){
-    res.end(content);
-
+    switch (req.url) {
+        case ('/'):
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(content);
+            break;
+        case ('/create'):
+            req.on('data', chunk => {
+                let data = JSON.parse('{"' + chunk.toString().replace(/&/g, '","').replace(/=/g, '":"') + '"}');
+                console.log(data);
+            });
+            res.writeHead(301, {
+                Location: `/`
+            }).end();
+            break;
+    }
 }).listen(8075);
